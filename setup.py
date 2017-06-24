73d7c0e3d6216db04e2055730a206c0d600c2ae7
Luka Čehovin <luka.cehovin@gmail.com>
2017-06-25 01:26:21 +0200

    Version increase

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='Ignition',
-	version='0.1.2',
+	version='0.1.3',
 	description='Run multiple programs in a specific order and monitor their state',
 	author='Luka Cehovin',
 	author_email='luka.cehovin@gmail.com',
